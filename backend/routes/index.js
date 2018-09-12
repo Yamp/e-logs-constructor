4d4991ffd352d21c15d3e90204826e832602b6e9
Kirill Khudyakov <hudiakov@gmail.com>
2018-09-12 15:12:00 +0300

    Improve action `save`

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -26,44 +26,31 @@ function getDirName() {
 }
 
 router.post('/save', function(req, res, next) {
-    console.log("POST запрос");
-    // let data = req.body;
-    // let responseData = '{"tables":[{"title":"first","name":"first","fields":[{"cell":"6pzVY_LrD","field_name":"s"},{"cell":"Xj-KaicRCL","field_name":"b"},{"cell":"P-njqhsCLB","field_name":"c"},{"cell":"15o_PgXasC","field_name":"d"},{"cell":"OSwI7YnQsX","field_name":"e"},{"cell":"jY_Bj9CxPX","field_name":"d"},{"cell":"bBvkovE-Vo","field_name":"m"},{"cell":"ftgE5OZb9e","field_name":"k"},{"cell":"vh0ZzUe6qv","field_name":"l"}],"html":"<p><br></p><table class=\"table table-bordered\"><tbody><tr><td><div class=\"cell\" field-name=\"s\" row-index=\"0\" id=\"6pzVY_LrD\">s</div></td><th><div class=\"cell\" field-name=\"b\" row-index=\"0\" id=\"Xj-KaicRCL\">b</div></th><td><div class=\"cell\" field-name=\"c\" row-index=\"0\" id=\"P-njqhsCLB\">c</div></td></tr><tr><td><div class=\"cell\" field-name=\"d\" row-index=\"0\" id=\"15o_PgXasC\">d</div></td><td><div class=\"cell\" field-name=\"e\" row-index=\"0\" id=\"OSwI7YnQsX\">e</div></td><td><div class=\"cell\" field-name=\"d\" row-index=\"0\" id=\"jY_Bj9CxPX\">d</div></td></tr><tr><td><div class=\"cell\" field-name=\"m\" row-index=\"0\" id=\"bBvkovE-Vo\">m</div></td><td><div class=\"cell\" field-name=\"k\" row-index=\"0\" id=\"ftgE5OZb9e\">k</div></td><td><div class=\"cell\" field-name=\"l\" row-index=\"0\" id=\"vh0ZzUe6qv\">l</div></td></tr></tbody></table><p><br></p>"}],"title":"one","name":"two"}'
-    let responseData = '{"tables":[{"title":"first","name":"first","html":"<p><br></p><table class=\"table table-bordered\">", "fields":[{"cell":"6pzVY_LrD","field_name":"s"},{"cell":"Xj-KaicRCL","field_name":"b"},{"cell":"P-njqhsCLB","field_name":"c"}]}],"title":"one","name":"two"}';
-    console.log("rdata: ", responseData);
-    let data = JSON.parse(responseData.replace("\\",""));
-    console.log("data: ", data);
+    let data = req.body;
     let hash = crypto.createHash('md5').update(JSON.stringify(data)).digest('hex');
-    console.log(hash);
     let dirPath = getDirName() + "/" + hash;
     mkdirSync(dirPath);
-    console.log("after mkdirSync");
     let tables = data.tables;
-
-    console.log("tables: ", tables);
-    tables.forEach(function(table, index) {
-        console.log("after each step");
-        let name = table["latinName"];
-        let html = table["html"];
-        let filepath = dirPath + "/" + name + ".html";
-        fs.writeFile(filepath, html, (err) => {
+    console.log("data: ", data);
+    for (let table of tables) {
+             table.name += ".html";
+        let filepath = dirPath + "/" + table.name;
+           fs.writeFile(filepath, table.html, (err) => {
             if (err) throw err;
-            console.log("The file was succesfully saved!");
+            console.log("The "+ table.name + ".html was saved!");
         });
-        data["tables"][index]["html"] = name + ".html";
-        console.log("Created file " + filepath)
-    });
+    }
+
     fs.writeFile(dirPath + "/meta.json", JSON.stringify(data), (err) => {
         if (err) throw err;
-        console.log("The file was succesfully saved!");
+        console.log("The meta.json was saved!");
     });
-
 
     zipFolder(dirPath, getDirName() +  "/journals/" + hash + '.journal', function(err) {
         if(err) {
             console.log('oh no!', err);
         } else {
-            console.log('EXCELLENT');
+            console.log('Journal was saved');
         }
     });
 
