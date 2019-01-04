8d1b05582e3b1b8ef9310ed152cb872956fb166a
Manshin Dmitry <ya.manshin@mail.ru>
2019-01-05 01:08:02 +0300

    Nested tables bug fixed

--- frontend/src/views/CreateTablePage.vue
+++ frontend/src/views/CreateTablePage.vue
@@ -307,13 +307,14 @@ export default {
                     maxHeight: null,
                     focus: true,
                     toolbar: [
+                        ['insert', ['table']],
                         ['style', ['bold', 'italic', 'underline', 'clear']],
                         ['font', ['strikethrough', 'superscript', 'subscript']],
                         ['fontsize', ['fontsize']],
                         ['color', ['color']],
                         ['para', ['ul', 'ol', 'paragraph']],
                         ['height', ['height']],
-                        ['insert', ['table', 'link', 'hr']],
+                        ['insert', ['link', 'hr']],
                         ['misk', ['undo', 'redo']],
                         ['view', ['fullscreen']]
                     ],
