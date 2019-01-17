3715bb3888c96e477d081f707cc2ee1a7ea30c0c
Manshin Dmitry <ya.manshin@mail.ru>
2019-01-17 16:10:25 +0300

    Hiding header added

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="header">
+    <div :class="{'header': true, 'header-hidden': isHidden}">
         <div class="title" @click="goHome">
             <span>Конструктор журналов</span>
         </div>
@@ -9,12 +9,23 @@
 <script>
     export default {
         name: "Header",
+        props: {
+          isHidden: Boolean
+        },
         methods: {
             goHome() {
                 if (this.journal) {
                     this.$router.push(`/journal/${this.journalName}${this.plant ? '?plant=' + this.plant : ''}`)
                 }
                 else this.$router.push('/')
+            },
+            mousemoveHandler (e) {
+                if (e.pageY <= 20 && this.isHidden) {
+                    this.$emit('showHeader')
+                }
+                else if (e.pageY > 50 && !this.isHidden) {
+                    this.$emit('hideHeader')
+                }
             }
         },
         computed: {
@@ -27,6 +38,12 @@
             plant () {
                 return this.$store.getters['journalState/getJournalPlant']
             }
+        },
+        mounted () {
+            $(document).on('mousemove', this.mousemoveHandler)
+        },
+        destroyed() {
+            $(document).off('mousemove', this.mousemoveHandler)
         }
     }
 </script>
@@ -41,7 +58,12 @@
         background-color: #337ab7;
         padding: 10px 20px;
         box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.4);
+        transition: .2s;
         z-index: 1000;
+
+        &.header-hidden {
+            transform: translateY(-50px);
+        }
 
         .title {
             font-family: 'Poiret One';
